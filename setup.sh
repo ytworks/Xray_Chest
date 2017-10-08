4ab5da00d8050157c7cfdd14ad7e559ffd01ecc0
ytworks <yuzo.takagi@gmail.com>
2017-10-09 08:27:51 +0900

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+git clone git@github.com:ytworks/LinearMotor.git
+git clone git@github.com:ytworks/ColorConstancy.git
+git clone https://github.com/ytworks/augmentation_tool
+git clone https://github.com/ytworks/DICOMReader
