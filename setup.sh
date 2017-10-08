95e443554745f1abe7f305ee532a78cad110dc12
ytworks <yuzo.takagi@gmail.com>
2017-10-09 08:31:38 +0900

    setup.

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ git clone git@github.com:ytworks/LinearMotor.git
 git clone git@github.com:ytworks/ColorConstancy.git
 git clone https://github.com/ytworks/augmentation_tool
 git clone https://github.com/ytworks/DICOMReader
+git clone git@github.com:ytworks/preprocessing_tool.git
