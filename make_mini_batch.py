cce4ae0ab3ef715069ec181a914c7f850296e9bd
ytworks <yuzo.takagi@gmail.com>
2017-11-09 20:31:46 +0900

    TBD make mini batch

--- make_mini_batch.py
+++ make_mini_batch.py
@@ -16,16 +16,91 @@ class DataSet(object):
         pass
 
 
-def read_data_sets(mode,
-                   nih_datapath,
-                   nih_supervised_datapath,
-                   nih_boxlist,
-                   benchmark_datapath,
-                   benchmark_supervised_datapath,
-                   kfold = 3,
+def get_filepath(datapaths):
+    files = []
+    for path in datapaths:
+        files.extend(glob.glob(path))
+    files = sorted(files)
+    return files
+
+
+def make_supevised_data_for_nih(path):
+    findings = {}
+    # ファイルの読み込み
+    with open(path, 'rU') as f:
+        lines = csv.reader(f)
+        lines.next()
+        for line in lines:
+            findings.setdefault(line[0], {'raw' : line[1]})
+    # データ数のカウント
+    finding_count = {}
+    for file_name, finding in findings.items():
+        for f in finding['raw'].split('|'):
+            finding_count.setdefault(f, 0)
+            finding_count[f] += 1
+    # バイナライズ
+    binary_def = [l for l in sorted(finding_count.keys()) if not l in 'No Finding']
+    for file_name, finding in findings.items():
+        label0 = np.zeros(len(binary_def))
+        label1 = np.zeros(2)
+        for i, b in enumerate(binary_def):
+            if finding['raw'].find(b) >= 0:
+                label0[i] = 1
+            if finding['raw'].find('No Finding') >= 0:
+                label1[0] = 1
+            else:
+                label1[1] = 1
+        findings[file_name].setdefault('label', np.array([label0, label1]))
+    return findings, finding_count, binary_def
+
+
+def read_data_sets(nih_datapath = ["./Data/Open/images/*.png"],
+                   nih_supervised_datapath = "./Data/Open/Data_Entry_2017.csv",
+                   nih_boxlist = "./Data/Open/BBox_List_2017.csv",
+                   benchmark_datapath = ["./Data/CR_DATA/BenchMark/*/*.dcm"],
+                   benchmark_supervised_datapath = "./Data/CR_DATA/BenchMark/CLNDAT_EN.txt",
+                   kfold = 1,
                    img_size = 512,
                    augment = True,
                    zca = True):
     class DataSets(object):
         pass
     data_sets = DataSets()
+
+    # NIHのデータセットのファイルパスを読み込む
+    nih_data = get_filepath(nih_datapath)
+
+    # 学会データセットのファイルパスを読み込む
+    conf_data = get_filepath(benchmark_datapath)
+
+    # NIHの教師データを読み込む
+    nih_labels, nih_count, label_def = make_supevised_data_for_nih(nih_supervised_datapath)
+    ## ラベルの種類を計測する
+    ## ファイルごとの所見を作る
+    ## Binarize
+
+    # 学会データの教師データを読み込む
+    ## ファイルごとの所見を作る
+    ## Binarize
+
+    # NIHのデータをラベルごとにファイルパスを分類する
+
+    # 訓練データとテストデータを分割する
+    ## 学会データをテストデータにする場合
+    ## NIHデータを分割してテストデータにする場合
+
+    # 訓練データのバッチ管理モジュール
+    # テストデータのバッチ管理モジュール
+
+    return data_sets
+
+if __name__ == '__main__':
+    dataset = read_data_sets(nih_datapath = ["./Data/Open/images/*.png"],
+                             nih_supervised_datapath = "./Data/Open/Data_Entry_2017.csv",
+                             nih_boxlist = "./Data/Open/BBox_List_2017.csv",
+                             benchmark_datapath = ["./Data/CR_DATA/BenchMark/*/*.dcm"],
+                             benchmark_supervised_datapath = "./Data/CR_DATA/BenchMark/CLNDAT_EN.txt",
+                             kfold = 1,
+                             img_size = 512,
+                             augment = True,
+                             zca = True)
