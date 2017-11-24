483306373dd42f617138feb95cc037ae19693b9c
ytworks <test@test.com>
2017-11-24 19:45:27 +0900

    stop test

--- run_debug.sh
+++ run_debug.sh
@@ -7,9 +7,10 @@ outfile='./Result/result.csv'
 epoch='1'
 batch='20'
 log='500'
-python main.py -mode learning -size $size \
-              -augment $augment \
-              -checkpoint $checkpoint \
-              -epoch $epoch \
-              -batch $batch \
-              -log $log
+#python main.py -mode learning -size $size \
+#              -augment $augment \
+#              -checkpoint $checkpoint \
+#              -epoch $epoch \
+#              -batch $batch \
+#              -log $log
+bash /home/ec2-user/git/mgmt/stop.sh
