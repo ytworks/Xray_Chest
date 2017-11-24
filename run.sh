103acbe7ffb49859c4cc87ba2c1bc56e1a69bd82
ytworks <test@test.com>
2017-11-24 18:09:56 +0900

    shell

---
+++ run.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/bash
+
+size='256'
+augment='True'
+checkpoint='./Model/run.ckpt'
+outfile='./Result/result.csv'
+epoch='600000'
+batch='32'
+log='1000'
+python main.py -mode learning -size $size \
+              -augment $augment \
+              -checkpoint $checkpoint \
+              -epoch $epoch \
+              -batch $batch \
+              -log $log
+            
