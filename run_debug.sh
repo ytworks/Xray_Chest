236b5d11727533c475a11d256e488efa27f94063
ytworks <test@test.com>
2017-11-24 19:19:19 +0900

    stop test

---
+++ run_debug.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+size='256'
+augment='True'
+checkpoint='./Model/run.ckpt'
+outfile='./Result/result.csv'
+epoch='1'
+batch='20'
+log='500'
+python main.py -mode learning -size $size \
+              -augment $augment \
+              -checkpoint $checkpoint \
+              -epoch $epoch \
+              -batch $batch \
+              -log $log
