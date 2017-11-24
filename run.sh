8a4c8c09d7ffbf0a822cb690afac01ca4682cae4
ytworks <test@test.com>
2017-11-24 19:18:19 +0900

    stop test

--- run.sh
+++ run.sh
@@ -4,12 +4,14 @@ size='256'
 augment='True'
 checkpoint='./Model/run.ckpt'
 outfile='./Result/result.csv'
-epoch='150000'
+epoch='1'
 batch='20'
-log='10'
+log='500'
 python main.py -mode learning -size $size \
               -augment $augment \
               -checkpoint $checkpoint \
               -epoch $epoch \
               -batch $batch \
               -log $log
+
+bash /home/ec2-user/git/mgmt/stop.sh
