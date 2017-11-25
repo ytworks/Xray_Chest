52411e0e92f42381ee25bb2519a5c8d2db92c6ce
ytworks <yuzo.takagi@gmail.com>
2017-11-25 12:42:06 +0900

    learning rate tuning exp

--- run.sh
+++ run.sh
@@ -7,12 +7,14 @@ checkpoint='./Model/run.ckpt'
 outfile='./Result/result.csv'
 epoch='30000'
 batch='20'
-log='500'
+log='10'
+lr='0.001'
 python main.py -mode learning -size $size \
               -augment $augment \
               -checkpoint $checkpoint \
               -epoch $epoch \
               -batch $batch \
-              -log $log
+              -log $log \
+              -lr $lr
 
 bash /home/ec2-user/git/mgmt/stop.sh
