d27968ba59dcc9e4b2cbb6c1dadeddb792281508
ytworks <test@test.com>
2017-11-27 14:39:57 +0900

    bug fix in sh

--- run.sh
+++ run.sh
@@ -10,12 +10,12 @@ batch='10'
 log='500'
 lr='0.001'
 python main.py -mode learning -size $size \
-#              -augment $augment \
               -checkpoint $checkpoint \
               -epoch $epoch \
               -batch $batch \
               -log $log \
               -lr $lr
+              
 FLAG=""
 while getopts d: OPT
 do
