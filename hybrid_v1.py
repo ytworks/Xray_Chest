04b19bf0aef1c2c571eebdfeac61de9ac763fd72
ytworks <yuzo.takagi@gmail.com>
2018-01-01 04:28:00 +0900

    chenge adaptive timing

--- hybrid_v1.py
+++ hybrid_v1.py
@@ -254,7 +254,7 @@ class Detecter(Core2.Core):
             rmax = min(1.0 + 2.0 * (40000.0 - float(self.steps)) / 40000.0, 3.0)
             dmax = min(5.0 * (25000.0 - float(self.steps)) / 25000.0, 5.0)
 
-        if self.current_loss > np.mean(self.val_losses) - np.std(self.val_losses) and len(self.val_losses) > 10 and is_update:
+        if self.current_loss > np.mean(self.val_losses) - np.std(self.val_losses) and len(self.val_losses) > 50 and is_update:
             logger.debug("Before Learning Rate: %g" % self.learning_rate_value)
             self.learning_rate_value = max(0.00001, self.learning_rate_value * 0.9)
             logger.debug("After Learning Rate: %g" % self.learning_rate_value)
