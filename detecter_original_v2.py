ec65f955d80cbeebb3b271a9d73f41cc3b6ac9e0
ytworks <yuzo.takagi@gmail.com>
2018-03-11 15:13:01 +0900

    tuning

--- detecter_original_v2.py
+++ detecter_original_v2.py
@@ -120,7 +120,7 @@ class Detecter(Core2.Core):
         Renormalization = True
         SE = False
         GrowthRate = 24
-        StemChannels = 96
+        StemChannels = 88
         prob = 1.0
         # dense net
         ## Stem
