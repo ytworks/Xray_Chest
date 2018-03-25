bef57a42504fe754f1521d53347e6ba73fae3c8b
ytworks <yuzo.takagi@gmail.com>
2018-03-25 13:40:49 +0900

    1024

--- detecter_original_v2.py
+++ detecter_original_v2.py
@@ -223,7 +223,7 @@ class Detecter(Core2.Core):
             logger.debug("After Learning Rate: %g" % self.learning_rate_value)
             #self.val_losses = []
         #if self.steps % 100 == 0 and self.steps != 0 and is_update:
-        if self.eval_l1_loss <= 0.0011 and self.steps != 0 and is_update:
+        if self.eval_l1_loss <= 0.0011 and self.steps % 100 == 0 and self.steps != 0 and is_update:
             self.l1_norm_value = min(1.0, self.l1_norm_value * 1.2) if not self.l1_norm_value == 0.0 else 0.00001
             #self.regularization_value = min(0.1, self.regularization_value * 5.0) if not self.regularization_value == 0.0 else 0.0001
             logger.debug("(Regularization, L1 Norm): %g %g" % (self.regularization_value, self.l1_norm_value))
