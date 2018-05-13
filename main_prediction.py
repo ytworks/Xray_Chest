0ea9dd8619453ca9f9b39a8a91af17f7d7fd68af
ytworks <yuzo.takagi@gmail.com>
2018-05-13 15:10:20 +0900

    add utils

--- main_prediction.py
+++ main_prediction.py
@@ -9,33 +9,11 @@ import csv
 import argparse
 import ConfigParser as cp
 import cv2
+from utils import *
 logger = getLogger(__name__)
 sh = StreamHandler()
 logger.addHandler(sh)
 logger.setLevel(10)
-
-def show_config(ini):
-    '''
-    設定ファイルの全ての内容を表示する（コメントを除く）
-    '''
-    for section in ini.sections():
-        print '[%s]' % (section)
-        show_section(ini, section)
-
-
-def show_section(ini, section):
-    '''
-    設定ファイルの特定のセクションの内容を表示する
-    '''
-    for key in ini.options(section):
-        show_key(ini, section, key)
-
-def show_key(ini, section, key):
-    '''
-    設定ファイルの特定セクションの特定のキー項目（プロパティ）の内容を表示する
-    '''
-    print '%s.%s =%s' % (section, key, ini.get(section, key))
-
 
 
 def main():
---
+++ utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+def show_config(ini):
+    '''
+    設定ファイルの全ての内容を表示する（コメントを除く）
+    '''
+    for section in ini.sections():
+        print '[%s]' % (section)
+        show_section(ini, section)
+
+
+def show_section(ini, section):
+    '''
+    設定ファイルの特定のセクションの内容を表示する
+    '''
+    for key in ini.options(section):
+        show_key(ini, section, key)
+
+def show_key(ini, section, key):
+    '''
+    設定ファイルの特定セクションの特定のキー項目（プロパティ）の内容を表示する
+    '''
+    print '%s.%s =%s' % (section, key, ini.get(section, key))
