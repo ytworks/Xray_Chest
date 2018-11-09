808b701aded503a28a1be0654c21c104650844bf
ytworks <test@test.com>
2018-11-09 10:12:19 +0900

    params survey

--- experiments/run.sh
+++ experiments/run.sh
@@ -5,10 +5,10 @@ config="./experiments/adamw_4.ini"
 CUDA_VISIBLE_DEVICES=0 python main.py -config $config > 20181109_adamw_4.out &
 
 config="./experiments/adamw_5.ini"
-CUDA_VISIBLE_DEVICES=1 python main.py -config $config > 20181109_adamw_4.out &
+CUDA_VISIBLE_DEVICES=1 python main.py -config $config > 20181109_adamw_5.out &
 
 config="./experiments/adamw_6.ini"
-CUDA_VISIBLE_DEVICES=2 python main.py -config $config > 20181109_adamw_4.out &
+CUDA_VISIBLE_DEVICES=2 python main.py -config $config > 20181109_adamw_6.out &
 
 config="./experiments/adamw_7.ini"
-CUDA_VISIBLE_DEVICES=3 python main.py -config $config > 20181109_adamw_4.out &
+CUDA_VISIBLE_DEVICES=3 python main.py -config $config > 20181109_adamw_7.out &
