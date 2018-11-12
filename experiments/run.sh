dc536b2fbc5183252f4febe1370d9260fb4fa1f6
ytworks <test@test.com>
2018-11-12 10:26:29 +0900

    experiments

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,14 +1,14 @@
 cd `dirname $0`
 cd ../
 
-config="./experiments/adamw_4.ini"
-nohup CUDA_VISIBLE_DEVICES=0 python main.py -config $config > 20181109_adamw_5_focal.out &
+config="./experiments/adamw_5_focal.ini"
+ CUDA_VISIBLE_DEVICES=0 nohup python main.py -config $config > 20181109_adamw_5_focal.out &
 
 config="./experiments/adamw_5.ini"
-nohup CUDA_VISIBLE_DEVICES=1 python main.py -config $config > 20181109_adamw_5.out &
+CUDA_VISIBLE_DEVICES=1 nohup python main.py -config $config > 20181109_adamw_5.out &
 
-config="./experiments/adamw_6.ini"
-nohup CUDA_VISIBLE_DEVICES=2 python main.py -config $config > 20181109_adamw_8.out &
+config="./experiments/adamw_8.ini"
+CUDA_VISIBLE_DEVICES=2 nohup python main.py -config $config > 20181109_adamw_8.out &
 
-config="./experiments/adamw_7.ini"
-nohup CUDA_VISIBLE_DEVICES=3 python main.py -config $config > 20181109_adamw_8_focal.out &
+config="./experiments/adamw_8_focal.ini"
+CUDA_VISIBLE_DEVICES=3 nohup python main.py -config $config > 20181109_adamw_8_focal.out &
