884661eae0aee350f2dfc44a3bcbe5c91b3edd3a
ytworks <test@test.com>
2018-11-15 15:15:52 +0900

    simple

--- NetworkModules.py
+++ NetworkModules.py
@@ -101,26 +101,10 @@ def pretrain_model(x, config, reuse=False, is_train=True):
     alpha = config.getfloat('DLParams', 'wc_alpha')
     p = trans.Transfer(x, 'densenet121', pooling=None, vname='transfer_Weight_Regularization',
                        trainable=True)
-    y51 = p.get_output_tensor()
-    pool4 = p['pool4_conv']
-    pool3 = p['pool3_conv']
-    pool2 = p['pool2_conv']
-    pool3 = tf.image.resize_images(images=pool3,
-                                   size=[tf.constant(14, tf.int32), tf.constant(14, tf.int32)],
-                                   method=tf.image.ResizeMethod.BICUBIC,
-                                   align_corners=False)
-    pool2 = tf.image.resize_images(images=pool2,
-                                   size=[tf.constant(14, tf.int32), tf.constant(14, tf.int32)],
-                                   method=tf.image.ResizeMethod.BICUBIC,
-                                   align_corners=False)
-    y51 = tf.image.resize_images(images=y51,
-                                   size=[tf.constant(14, tf.int32), tf.constant(14, tf.int32)],
-                                   method=tf.image.ResizeMethod.BICUBIC,
-                                   align_corners=False)
-    feature = Layers.concat(xs=[y51, pool2, pool3, pool4], concat_type='Channel')
+    feature = p.get_output_tensor()
     m_size = 128
     tsl = Layers.convolution2d(x=feature,
-                               FilterSize=[1, 1, 1024+512+256+128, 15 * m_size],
+                               FilterSize=[1, 1, 1024, 15 * m_size],
                                Initializer='He',
                                Strides=[1, 1],
                                Padding='SAME',
@@ -133,7 +117,7 @@ def pretrain_model(x, config, reuse=False, is_train=True):
                                Training=False,
                                vname='transfer_conv',
                                Is_log=False)
-    tsl = SE_module(x=tsl, InputNode=[14,14,15*m_size], Act='Relu', Rate=0.5, vname='SE')
+    tsl = SE_module(x=tsl, InputNode=[7,7,15*m_size], Act='Relu', Rate=0.5, vname='SE')
     cwp = Layers.class_wise_pooling(x=tsl, n_classes=15, m=m_size)
     print(cwp)
     z = Layers.spatial_pooling_dif(x=cwp, k_train=k_size, k_test=k_size, alpha=alpha, is_train=is_train)
