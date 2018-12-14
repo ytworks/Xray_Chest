54a12db69e6ed7b12e2047b6aaa7da8b5bd2195e
ytworks <test@test.com>
2018-12-14 10:09:27 +0900

    bug fix

--- detection.py
+++ detection.py
@@ -211,8 +211,7 @@ class Detector(Core2.Core):
                                             'DLParams', 'nesterov'),
                                         weight_decay=self.wd)
             logger.debug("03-01: Optimizer definition")
-            xs = tf.split(self.x, [], num=self.gpu_num)
-            z_s = tf.split(self.z_, [], num=self.gpu_num)
+            xs, z_s = tf.split(zip(self.x, self.z_), [-1]*self.gpu_num, num=self.gpu_num)
             logger.debug("03-03: Data split")
             tower_grads = []
             self.losses, self.logits, self.y51s = [], [], []
